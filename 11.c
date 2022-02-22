d822d2e236b5a8a8abc79e161a829fbbf2e596be
Henry Peaurt <matthpmoreira@gmail.com>
2022-02-22 09:20:16 -0300

    Finished updating 11.c

--- 11.c
+++ 11.c
@@ -1,28 +1,25 @@
 // Advent of Code 2021 - Day 11
 // Written by Henry Peaurt
 
-/* Just found out about pattern matching, but I decided not to learn it right now. I tried doing that thing where you
- * put spaces until every field of a statement is aligned, but I'm not sure if that was a good idea */
+/* Just found out about pattern matching, but I decided not to learn it right now. Also, there's the new memory
+ * management header, but I didn't need to allocate anything this time */
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include "strmanip.h"
-#include "memmanage.h"
-
 // Constants
-#define FLASHED	-10	// You can check which octopi flashed if they're below 0
 #define LEFT	x > 0
 #define RIGHT	x < 9
 #define ABOVE	y > 0
 #define BELOW	y < 9
 
 // Function declarations
-void get_octos   (int oct[10][10]);
-void step_octos  (int oct[10][10]);
-void flash_octos (int oct[10][10]);
-int  zero_octos  (int oct[10][10]);
+void get_octos(int oct[10][10]);
+void step_octos(int oct[10][10]);
+void flash_octos(int oct[10][10]);
+int zero_octos(int oct[10][10]);
 
 int main(void)
 {
@@ -30,8 +27,8 @@ int main(void)
 
 	get_octos(octos);
 
-	int   step_cnt = 0;
-	int   flash_cnt = 0;
+	int step_cnt = 0;
+	int flash_cnt = 0;
 	_Bool part1 = false;
 	_Bool part2 = false;
 
@@ -42,16 +39,6 @@ int main(void)
 		int tmp_cnt = zero_octos(octos);
 		flash_cnt += tmp_cnt;
 		++step_cnt;
-
-		if (step_cnt <= 3) {
-			for (int y = 0; y < 10; ++y) {
-				for (int x = 0; x < 10; ++x)
-					printf("%u ", octos[x][y]);
-				putchar('\n');
-			}
-			putchar('\n');
-			fflush(stdout);
-		}
 
 		if (step_cnt == 100) {
 			printf("Part One: %u\n", flash_cnt);
@@ -87,21 +74,22 @@ void step_octos(int oct[10][10])
 
 void flash_octos(int oct[10][10])
 {
-rerun:
 	for (int y = 0; y < 10; ++y) {
 		for (int x = 0; x < 10; ++x) {
 			if (oct[x][y] >= 10) {
-				oct[x][y]	  = FLASHED;
-				oct[x][y - 1]	  += ABOVE;
+				oct[x][y]	  = -10; // You can check which octopi flashed if they're below 0
+
 				oct[x - 1][y]	  += LEFT;
 				oct[x + 1][y]	  += RIGHT;
+				oct[x][y - 1]	  += ABOVE;
 				oct[x][y + 1]	  += BELOW;
 				oct[x - 1][y - 1] += LEFT && ABOVE;
-				oct[x + 1][y + 1] += RIGHT && ABOVE;
+				oct[x + 1][y - 1] += RIGHT && ABOVE;
 				oct[x - 1][y + 1] += LEFT && BELOW;
 				oct[x + 1][y + 1] += RIGHT && BELOW;
 
-				goto rerun;
+				x = -1; // Reset the checking
+				y = 0;
 			}
 		}
 	}
