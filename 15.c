23bc5510a39049bb87ad8bded52bf2d7bdb929e1
Henry Peaurt <matthpmoreira@gmail.com>
2022-02-23 11:07:10 -0300

    oops

--- 15.c
+++ 15.c
@@ -1,31 +1,22 @@
-// Advent of Code 2021 - Day 15
-// Written by Henry Peaurt
+/* Advent of Code 2021 - Day 15 *
+ * Written by Henry Peaurt	*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-// Types
-typedef char* path;
-typedef struct node {
-	int x;
-	int y;
-	int 
+/* Types */
+typedef struct {
+	int visits;
 } node;
 
+typedef struct {
 
-// Global variables
-int *grid, x, y;
-node **open, **closed;
-int open_amount, closed_amount;
-
-
-// Function declarations
+/* Function declarations */
 static void getGrid(void);
 
-
-int main(void)
+int
+main(void)
 {
 	getGrid();
 
@@ -68,6 +59,32 @@ static path findPath(void)
 
 }
 
+unsigned int
+pathfind(cave *curr, int max_vst, _Bool vstd_twice)
+{
+	if (strcmp(curr->name, "end") == 0)
+		return 1;
+
+	if (
+		curr->is_small && curr->visits >= 1 &&
+		(strcmp(curr->name, "start") == 0 || max_vst == 1 || vstd_twice)
+	   )
+		return 0;
+
+	++curr->visits;
+	vstd_twice = vstd_twice || (curr->is_small && max_vst == 2 && curr->visits >= 2);
+
+	unsigned int found_paths = 0;
+
+	for (int i = 0; i < curr->conn_cnt; ++i) {
+		found_paths += pathfind(curr->conn_list[i], max_vst, vstd_twice);
+	}
+
+	--curr->visits; /* The number of visits doesn't increase past 2, so it subs 1 after the twice-visited cave is
+			 * processed and backtracks to tell the next paths the cave hasn't been visited twice */
+
+	return found_paths;
+}
 
 static void processNode(node *a)
 {
