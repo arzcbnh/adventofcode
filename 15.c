557729ef7a1c46b7dfd27e358806e5e638007e08
Henry Peaurt <matthpmoreira@gmail.com>
2022-02-25 06:07:29 -0300

    ...

--- 15.c
+++ 15.c
@@ -1,13 +1,18 @@
 /* Advent of Code 2021 - Day 15 *
  * Written by Henry Peaurt	*/
 
+/* I had previously got stuck on this program, and took around a month-long break from AoC because of it. It took me *
+ * quit some time to understand Djikstra's algorithm, and although it's horribly inefficient, I'm very proud of      *
+ * learning it and I'm grateful that AoC gave me this opportunity. I drafted a second, even more inefficient program *
+ * that produces jpg images for each state of the pathfinding, but it was so bad and so slow that it crashed 4 nodes *
+ * before completion, after running for an hour and a half. I decided to upgrade the program by learning the A*      *
+ * algorithm and it turned out amazing!										     */
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
 #include "strmanip.h"
-#include "memmanage.h"
 
 /* Types */
 typedef struct {
@@ -20,6 +25,7 @@ typedef struct {
 
 typedef struct {
 	node map[500][500];
+	node* queue[250000];
 	int size; /* The only purpose of this variable is so the program works with the test */
 } cntxt;
 
@@ -29,57 +35,44 @@ void expand_map(cntxt* c);
 void copy_right(cntxt* c, int x, int y);
 void copy_down(cntxt* c, int new_x, int y);
 void init_nodes(cntxt* c);
-void pathfind(cntxt* c);
-node* get_shortest(cntxt* c);
+void pathfind(cntxt* c, node* n);
+void update_neighbour(node *n, node* ngb);
+void update_queue(cntxt* c, node* n);
+int find_old_index(cntxt* c, node* n);
+int find_new_index(cntxt* c, node* n);
 
 int
 main(void)
 {
 	cntxt c = {
 		.map = {{{ 0 }}},
+		.queue = { 0 },
 		.size = 0
 	};
 
 	input_map(&c);
 	expand_map(&c);
-	init_nodes(&c);
-	pathfind(&c);
 
-	node *n = get_shortest(&c);
+	for (int i = 0; i < 2; ++i) {
+		init_nodes(&c);
+		c.queue[0] = &(c.map[0][0]);
 
-	printf("Part 1: %i\n", n->total_risk);
-	
-	c.size *= 5;
-	init_nodes(&c);
-	pathfind(&c);
+		node* n = c.queue[0];
 
-	n = get_shortest(&c);
+		/* c.size stores the actual dimension, not the max index, so you need to sub 1 */
+		while (n->x != c.size - 1 || n->y != c.size - 1) {
+			pathfind(&c, n);
+			n = c.queue[0];
+			printf("%i %i %i\n", n->x, n->y, n->total_risk);
+			for (int i = 0; c.queue[i] != NULL; i++)
+				printf("(%i,%i), ", c.queue[i]->x, c.queue[i]->y);
+			putchar('\n');
+		}
 
-	printf("Part 2: %i\n", n->total_risk);
-
-	/*
-	for (int y = 0; y < c.size; ++y) {
-		for (int x = 0; x < c.size; ++x)
-			printf("%i", c.map[x][y].risk);
-
-		putchar('\n');
+		printf("Part %i: %i\n", i + 1, n->total_risk);
+		n = &(c.map[0][0]);
+		c.size *= 5;
 	}
-	putchar('\n');
-	for (int y = 0; y < c.size; ++y) {
-		for (int x = 0; x < c.size; ++x)
-			printf("%i", c.map[x][y].visited);
-
-		putchar('\n');
-	}
-	putchar('\n');
-	for (int y = 0; y < c.size; ++y) {
-		for (int x = 0; x < c.size; ++x)
-			printf("%i", c.map[x][y].total_risk);
-
-		putchar('\n');
-	}
-	putchar('\n');
-	*/
 
 	return 0;
 }
@@ -88,18 +81,16 @@ void
 input_map(cntxt* c)
 {
 	char* s = str_input();
-	size_t len = strlen(s);
+	c->size = (int) strlen(s);
 	int y = 0;
 
 	while (s != NULL) {
-		for (size_t x = 0; x < len; x++)
+		for (int x = 0; x < c->size; x++)
 			c->map[x][y].risk = s[x] - '0';
 
 		y++;
 		s = str_input();
 	}
-
-	c->size = (int) len;
 }
 
 void
@@ -167,56 +158,79 @@ init_nodes(cntxt* c)
 }
 
 void
-pathfind(cntxt* c)
+pathfind(cntxt* c, node* n)
 {
-	node *n = &(c->map[0][0]);
-	int sz = c->size - 1; /* c.size stores the actual size, not the max index, so you have to sub 1 */
+	int sz = c->size - 1;
+	int x = n->x;
+	int y = n->y;
 
-	while (n->x != sz || n->y != sz) {
-		int x = n->x;
-		int y = n->y;
+	n->visited = true;
+	for (int i = 0; c->queue[i] != NULL; i++)
+		c->queue[i] = c->queue[i + 1];
 
-		n->visited = true;
+	node* neighbours[4] = {
+		(x > 0 ? &(c->map[x - 1][y]) : NULL),
+		(y > 0 ? &(c->map[x][y - 1]) : NULL),
+		(x < sz ? &(c->map[x + 1][y]) : NULL),
+		(y < sz ? &(c->map[x][y + 1]) : NULL)
+	};
 
-		node* neighbours[4] = {
-			(x > 0 ? &(c->map[x - 1][y]) : NULL),
-			(y > 0 ? &(c->map[x][y - 1]) : NULL),
-			(x < sz ? &(c->map[x + 1][y]) : NULL),
-			(y < sz ? &(c->map[x][y + 1]) : NULL)
-		};
+	for (int i = 0; i < 4; ++i) {
+		node* ngb = neighbours[i];
 
-		for (int i = 0; i < 4; i++) {
-			node *ngb = neighbours[i];
+		if (ngb == NULL || ngb->visited)
+			continue;
 
-			if (ngb == NULL || ngb->visited)
-				continue;
-
-			int tmp = ngb->risk + n->total_risk;
-
-			if (tmp < ngb->total_risk || ngb->total_risk == -1)
-				ngb->total_risk = tmp;
-		}						
-
-		n = get_shortest(c);
+		update_neighbour(n, ngb);
+		update_queue(c, ngb);
 	}
 }
 
-node*
-get_shortest(cntxt* c)
+void
+update_neighbour(node* n, node* ngb)
 {
-	node* n = NULL;
+	int tmp = ngb->risk + n->total_risk;
 
-	for (int y = 0; y < c->size; y++) {
-		for (int x = 0; x < c->size; x++) {
-			node *cmp = &(c->map[x][y]);
+	if (tmp < ngb->total_risk || ngb->total_risk == -1)
+		ngb->total_risk = tmp;
+}
 
-			if (
-				cmp->total_risk > 0 && cmp->visited == false &&
-				(n == NULL || n->total_risk > cmp->total_risk)
-			   )
-				n = cmp;
-		}
+void
+update_queue(cntxt* c, node* n)
+{
+	int old = find_old_index(c, n);
+	int new = find_new_index(c, n);
+	int factor = (old < new) - (old > new); 
+
+	/* Kinda confusing, but it goes up or down depending on the position of the old and new indexes */
+	for (int i = old; i != new; i += factor)
+		c->queue[i] = c->queue[i + factor];
+
+	c->queue[new] = n;
+}
+
+int
+find_old_index(cntxt* c, node* n)
+{
+	int i = 0;
+
+	for (; c->queue[i] != NULL; i++) {
+		if (c->queue[i]->x == n->x && c->queue[i]->y == n->y)
+			break;
 	}
 
-	return n;
+	return i;
+}
+
+int
+find_new_index(cntxt* c, node* n)
+{
+	int i = 0;
+
+	for (; c->queue[i] != NULL; i++) {
+		if (c->queue[i]->total_risk >= n->total_risk)
+			break;
+	}
+
+	return i;
 }
