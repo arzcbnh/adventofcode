a2a1090a7e21abe22823162fbe628a2204cd5afe
Henry Peaurt <matthpmoreira@gmail.com>
2022-02-26 17:39:33 -0300

    Kinda polished 17.c

--- 17.c
+++ 17.c
@@ -1,12 +1,15 @@
 // Advent of Code 2021 - Day 17
 // Written by Henry Peaurt
 
-// I confess to spoiling myself the Part 1 formula, but after trying some really complex math and realizing it's so
-// simple, I can't help but think this is one of the easiest programs to make. I just bruteforced everything into main
-// and reorganized the program later.
+// I confess to spoiling myself the Part 1 formula, but after trying some really complex math and realizing it's just
+// n-sum and Bhaskara, I can't help but think this is one of the easiest programs to make. I just bruteforced
+// everything into main and reorganized the program later. I don't know whether I feel thankful for how easy it was or
+// annoyed because it was very boring. However, the program looks like crap and hard to understand.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 #include "strmanip.h"
 #include "memmanage.h"
@@ -18,9 +21,11 @@ typedef struct {
 	int min_y;
 	int max_y;
 } cntxt;
+
 // Function declarations
 void input_target(cntxt* c);
 int calc_shots(cntxt* c);
+bool check_landing(cntxt* c, int x_vel, int y_vel);
 
 int
 main(void)
@@ -35,8 +40,7 @@ main(void)
 	input_target(&c);
 
 	printf("Part 1: %u\n", (-c.min_y - 1) * (-c.min_y) / 2);
-
-	calc_shots(&c);
+	printf("Part 2: %u\n", calc_shots(&c));
 
 	return 0;
 }
@@ -55,38 +59,33 @@ input_target(cntxt* c)
 int
 calc_shots(cntxt* c)
 {
-	int min_x_v = 0;
-	int max_x_v = c->max_x;
-	int min_y_v = c->min_y;
-	int max_y_v = -c->min_y;
 	int cnt = 0;
 
-	while (min_x_v * (min_x_v + 1) / 2 < c->min_x)
-		min_x_v++;
-
-	for (int x = min_x_v; x <= max_x_v; x++) {
-		for (int y = min_y_v; y <= max_y_v; y++) {
-			int v_x = x;
-			int v_y = y;
-			int i = 0;
-			int j = 0;
-
-			while (i <= c->max_x && j >= c->min_y) {
-				if (c->min_x <= i && i <= c->max_x && c->min_y <= j && j <= c->max_y) {
-				printf("%i %i\n", x, y);
-					++cnt;
-					break;
-				}
-
-				i += v_x;
-				v_x -= v_x > 0;
-				j += v_y;
-				v_y--;
-			}
-		}
+	for (int x_vel = sqrt(c->min_x * 4 + 1) / 2; x_vel <= c->max_x; x_vel++) {
+		for (int y_vel = c->min_y; y_vel <= -c->min_y; y_vel++)
+			cnt += check_landing(c, x_vel, y_vel);
 	}
 
-	printf("%i %i %i %i %i\n", min_x_v, max_x_v, min_y_v, max_y_v, cnt);
+	return cnt;
+}
 
-	return 0;
+bool
+check_landing(cntxt* c, int x_vel, int y_vel)
+{
+	int x = 0;
+	int y = 0;
+	int curr_x_vel = x_vel;
+	int curr_y_vel = y_vel;
+
+	while (x <= c->max_x && y >= c->min_y) {
+		if (c->min_x <= x && x <= c->max_x && c->min_y <= y && y <= c->max_y)
+			return true;
+
+		x += curr_x_vel;
+		curr_x_vel -= curr_x_vel > 0;
+		y += curr_y_vel;
+		curr_y_vel--;
+	}
+
+	return false;
 }
