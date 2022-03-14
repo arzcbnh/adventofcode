291b95c4e4940c68df46601be963ed04ecab3cc0
Henry Peaurt <matthpmoreira@gmail.com>
2022-03-14 07:12:47 -0300

    Finished 21.c. It's mostly a copy, but at least I studied the algorithm and know how it works. In parts.

--- !21.c
+++
@@ -1,7 +0,0 @@
-// Advent of Code 2021 - Day 21
-// Written by Henry Peaurt
-
-// Why the hell doesn't it work. Literal trash. I'm never programming again.
-// Note from later: I half-copied another solution and that's it, whatever.
-
-erased cuz i'm going crazy
