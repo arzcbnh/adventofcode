4e69cad45f1f3527d30e843c15441d14381362d2
Henry Peaurt <>
2022-03-17 05:13:18 -0300

    Polished day 01

--- 2018/01.lua
+++ 2018/01.lua
@@ -1,29 +1,31 @@
-frequency = 0;
-for l in io.lines() do
-	frequency = frequency + l
-end
+io.write("-- Day 01 --\n")
 
-io.write("Part 1: ", frequency, "\n")
+freq = 0
+count = {}
+part1 = false
+repeated = false
 
-found = {}
-common = false
-frequency = 0;
 ::redo::
 io.input():seek("set")
+
 for l in io.lines() do
-	frequency = frequency + l
+	freq = freq + l
+	count[freq] = count[freq] or 0
+	count[freq] = count[freq] + 1
 
-	found[frequency] = found[frequency] or 0
-	found[frequency] = found[frequency] + 1
-
-	if found[frequency] > 1 then
-		common = frequency
+	if count[freq] > 1 then
+		repeated = freq
 		break
 	end
 end
 
-if not common then
+if not part1 then
+	part1 = freq
+end
+
+if not repeated then
 	goto redo
 end
 
-io.write("Part 2:", common, "\n")
+io.write("Part 1: ", part1, "\n")
+io.write("Part 2: ", repeated, "\n")
