a70ab5671d230c27c617e636f1dbd0fe23f22b66
Henry Peaurt <>
2022-03-17 05:34:00 -0300

    Removed stupid goto

--- 2018/01.lua
+++ 2018/01.lua
@@ -5,26 +5,23 @@ count = {}
 part1 = false
 repeated = false
 
-::redo::
-io.input():seek("set")
+while not repeated do
+	io.input():seek("set")
 
-for l in io.lines() do
-	freq = freq + l
-	count[freq] = count[freq] or 0
-	count[freq] = count[freq] + 1
+	for l in io.lines() do
+		freq = freq + l
+		count[freq] = count[freq] or 0
+		count[freq] = count[freq] + 1
 
-	if count[freq] > 1 then
-		repeated = freq
-		break
+		if count[freq] > 1 then
+			repeated = freq
+			break
+		end
 	end
-end
 
-if not part1 then
-	part1 = freq
-end
-
-if not repeated then
-	goto redo
+	if not part1 then
+		part1 = freq
+	end
 end
 
 io.write("Part 1: ", part1, "\n")
