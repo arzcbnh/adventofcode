3719baef7839c55edf8dbfeecf7c4bdd16a43606
Henry Peaurt <matthpmoreira@gmail.com>
2022-03-18 05:21:45 -0300

    New input function for utils.lua

--- 2018/utils.lua
+++ 2018/utils.lua
@@ -1,7 +1,22 @@
-function make_counter()
+local utils = {}
+
+local function make_counter()
 	local counter = 0
-	return function ()
-		counter = counter + 1
+	return function (n)
+		counter = counter + (n or 1)
 		return counter
 	end
 end
+
+function utils.input(fn)
+	t = {}
+	fn = fn or function (a) return a end
+
+	for l in io.lines() do
+		t[#t + 1] = fn(l)
+	end
+
+	return t
+end
+
+return utils
