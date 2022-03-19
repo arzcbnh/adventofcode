4943292640efbe000c1341f0f09957ceb207f40a
Henry Peaurt <matthpmoreira@gmail.com>
2022-03-19 04:31:49 -0300

    Semifinal polish

--- 2018/02.lua
+++ 2018/02.lua
@@ -1,27 +1,43 @@
 io.write("-- Day 02 --\n")
 
-utils = require "utils"
+local utils = require "utils"
+local data = utils.input()
 
-function calc_repeating()
-	local double_l = 0
-	local triple_l = 0
+local function letters_matching(id1, id2)
+	local iter1 = id1:gmatch(".")
+	local iter2 = id2:gmatch(".")
+	local match = {}
+	local count = 0
 
-	for _, l in ipairs(data) do
-		local twice, thrice = char_repeats(l)
+	for i = 1, #id1 do
+		local c1 = iter1()
+		local c2 = iter2()
 
-		double_l = double_l + (twice and 1 or 0)
-		triple_l = triple_l + (thrice and 1 or 0)
+		if c1 == c2 then
+			count = count + 1
+			match[#match + 1] = c1
+		end
 	end
 
-	return double_l * triple_l
+	return count, match
 end
 
-function char_repeats(s)
+local function find_id()
+	-- Might look confusing because it's ptimized to never check previous ids nor the last one
+	for i = 1, #data - 1 do
+		for j = i + 1, #data do
+			local count, match = letters_matching(data[i], data[j])
+			if count == #data[i] - 1 then return table.concat(match) end
+		end
+	end
+end
+
+local function char_repeats(id)
 	local freq2 = false
 	local freq3 = false
 	local count = {}
 
-	for c in s:gmatch('.') do
+	for c in id:gmatch('.') do
 		local i = c:byte() - 96
 
 		count[i] = count[i] or 0
@@ -38,38 +54,19 @@ function char_repeats(s)
 	return freq2, freq3
 end
 
-function find_id()
-	local matched = false
-	local id1 = nil
-	local id2 = nil
+local function count_repeating()
+	local double_l = 0
+	local triple_l = 0
 
-	for i = 1, #data do
-		for j = i + 1, #data do
-			local count, id = letter_missing(data[i], data[j])
-			if count == #data[i] - 1 then return table.concat(id) end
-		end
-	end
-end
+	for _, l in ipairs(data) do
+		local twice, thrice = char_repeats(l)
 
-function letter_missing(s1, s2)
-	local iter1 = s1:gmatch('.')
-	local iter2 = s2:gmatch('.')
-	local same_l = {}
-	local count = 0
-
-	for i = 1, #s1 do
-		local c = iter1
-
-		if c == iter2() then
-			count = count + 1
-			same_l[#same_l + 1] = c
-		end
+		double_l = double_l + (twice and 1 or 0)
+		triple_l = triple_l + (thrice and 1 or 0)
 	end
 
-	return count, same_l
+	return double_l * triple_l
 end
 
-data = utils.input()
-
-io.write("Part 1: ", calc_repeating(), '\n')
+io.write("Part 1: ", count_repeating(), '\n')
 io.write("Part 2: ", find_id(), '\n')
